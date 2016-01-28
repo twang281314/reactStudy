234893021781a83654bf56d8dd8e1852df028075
twang281314 <twang281314@gmail.com>
2016-01-29 00:21:01 +0800

    update

--- antDesign/main.js
+++ antDesign/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/component/App.jsx'
+import App from '../src/component/App.jsx'
 
 ReactDOM.render(){
     <div>anytao</div>,document.getElementsById('mountNode')
